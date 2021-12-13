028d2820cca291c9f3fdaef69531fb6e46747a36
druckhead <57668763+druckhead@users.noreply.github.com>
2021-12-13 15:34:40 +0200

    delete 2 unused funcs, add 3 new helper funcs

--- sequence.h
+++ sequence.h
@@ -1,7 +1,8 @@
 int word_val(char* word);
 int char_val(char ch);
-void reset_word(char* word);
-int let_val(char ch);
+int atbash_char(char ch);
+char* atbash_wrd(char *word);
+char* reverse_str(char* word);
 void print_gimatria(char* word, char* txt);
 void print_atbash(char* word, char* txt);
 void print_anagram(char* word, char* txt);
