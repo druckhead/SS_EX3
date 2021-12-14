6be725120fb1b8f56ef2e655c6135682998ba4a2
druckhead <57668763+druckhead@users.noreply.github.com>
2021-12-14 14:03:19 +0200

    added remove_whtspace func

--- sequence.h
+++ sequence.h
@@ -1,8 +1,12 @@
 int word_val(char* word);
 int char_val(char ch);
+
 int atbash_char(char ch);
 char* atbash_wrd(char *word);
+
+char* remove_whtspc(const char* word, int c);
 char* reverse_str(char* word);
+
 void print_gimatria(char* word, char* txt);
 void print_atbash(char* word, char* txt);
 void print_anagram(char* word, char* txt);
